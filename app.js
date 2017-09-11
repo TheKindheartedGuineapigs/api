35a6f1415dcaee441f878f1a899567eec87957d6
vixataaa <vmetodiev93@gmail.com>
2017-09-12 02:54:02 +0300

    lel

--- app.js
+++ app.js
@@ -3,18 +3,8 @@ const logger = require('./config/logger-conf');
 
 const app = require('./config')(logger);
 
-const https = require('https');
-const fs = require('fs');
-const options = { 
-    key: fs.readFileSync('./server-key.pem'),
-    cert: fs.readFileSync('./server-crt.pem'),
-    ca: fs.readFileSync('./ca-crt.pem'),
-};
-
 app.then((application) => {
     application.listen(config.port, () =>
         logger.debug('Express server listening on port ' + config.port)
     );
-
-    https.createServer(options, application).listen(443);
 });
